96da6386c6a7176c60755f05e51958834480aa96
BRIAN SWARTZ <bdswartz72@gmail.com>
2022-05-09 10:40:17 -0400

    added large photo modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,9 +1,19 @@
 import React, {useState} from 'react'
+import Modal from '../Modal';
 
 // import photo from '../../assets/small/commercial/0.jpg';
 
 
 function PhotoList({category}) {
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(true);
+  }
+
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
     const [photos] = useState([
         {
           name: 'Grocery aisle',
@@ -106,12 +116,14 @@ function PhotoList({category}) {
 
   return (
     <div>
+       {isModalOpen && <Modal currentPhoto={currentPhoto} />}
         <div className="flex-row">
             {currentPhotos.map((image, i) => (
                 <img
                     src={require(`../../assets/small/${category}/${i}.jpg`)}
                     alt={image.name}
                     className="img-thumbnail mx-1"
+                    onClick={() => toggleModal(image, i)}
                     key={image.name}
                 />
             ))}
